948db9cb95db8caa7d43676e7230b87c5ba0f212
Eric Ni <ericni@ericnimacair2015.wireless.nyumc.org>
2015-09-30 14:36:28 -0400

    genres

--- p1/fresh_tomatoes.py
+++ p1/fresh_tomatoes.py
@@ -123,7 +123,7 @@ movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
-    <h9>{movie_title}</h9>
+    <h9>{movie_genres}</h9>
 </div>
 '''
 
@@ -144,7 +144,8 @@ def create_movie_tiles_content(movies):
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
+            movie_genres=movie.genres
         )
     return content
 
--- p1/media.py
+++ p1/media.py
@@ -1,7 +1,8 @@
 class Movie():
     #a data structure representing a movie and its descriptors
-    def __init__(self, title, poster_image_url, trailer_youtube_url):
+    def __init__(self, title, poster_image_url, trailer_youtube_url, genres):
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
+        self.genres = genres
         
