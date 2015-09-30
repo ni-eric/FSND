df6a58fc78edcae79b6dd4f0a3c22ef104ee8b2c
Eric Ni <ericni@ericnimacair2015.wireless.nyumc.org>
2015-09-30 15:03:20 -0400

    final

--- p1/media.py
+++
@@ -1,8 +0,0 @@
-class Movie():
-    #a data structure representing a movie and its descriptors
-    def __init__(self, title, poster_image_url, trailer_youtube_url, genres):
-        self.title = title
-        self.poster_image_url = poster_image_url
-        self.trailer_youtube_url = trailer_youtube_url
-        self.genres = genres
-        
