f2bd8e40d16a55664806b7639bcba684757155c7
Eric Ni <ericni@EricNiMacAir2015.local>
2015-12-02 14:34:41 -0500

    delete, edit functionality

--- project3/initialize.py
+++ project3/initialize.py
@@ -1,9 +1,41 @@
 from project import *
+# This script will generate some placeholder items for quickly testing
+# excuse my uncreativity
 
 clearDatabase()
-addItem("apple", "fruit", "red")
-addItem("apple", "fruit", "red")
-addItem("apple", "fruit", "red")
-addItem("apple", "fruit", "red")
-addItem("apple", "fruit", "red")
-addItem("apple", "fruit", "red")
+placeholdtxt = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+addItem("item1", "category1", placeholdtxt)
+addItem("item2", "category1", placeholdtxt)
+addItem("item3", "category1", placeholdtxt)
+addItem("item4", "category1", placeholdtxt)
+addItem("item5", "category2", placeholdtxt)
+addItem("item6", "category2", placeholdtxt)
+addItem("item7", "category2", placeholdtxt)
+addItem("item8", "category2", placeholdtxt)
+addItem("item9", "category2", placeholdtxt)
+addItem("item10", "category3", placeholdtxt)
+addItem("item11", "category3", placeholdtxt)
+addItem("item12", "category3", placeholdtxt)
+addItem("item13", "category3", placeholdtxt)
+addItem("item14", "category4", placeholdtxt)
+addItem("item15", "category4", placeholdtxt)
+addItem("item16", "category4", placeholdtxt)
+addItem("item17", "category4", placeholdtxt)
+addItem("item18", "category4", placeholdtxt)
+addItem("item19", "category4", placeholdtxt)
+addItem("item20", "category5", placeholdtxt)
+addItem("item21", "category5", placeholdtxt)
+addItem("item22", "category6", placeholdtxt)
+addItem("item23", "category7", placeholdtxt)
+addItem("item24", "category8", placeholdtxt)
+addItem("item25", "category8", placeholdtxt)
+addItem("item26", "category8", placeholdtxt)
+addItem("item27", "category8", placeholdtxt)
+addItem("item28", "category9", placeholdtxt)
+addItem("item29", "category9", placeholdtxt)
+addItem("item30", "category9", placeholdtxt)
+addItem("item31", "category9", placeholdtxt)
+addItem("item32", "category9", placeholdtxt)
+
+print "generated some placeholder items, category, and descriptions"
--- project3/project.py
+++ project3/project.py
@@ -1,10 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
-
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
+from collections import namedtuple
+
+app = Flask(__name__)
 
 
 def connect():
+    # returns connection and cursor for psql database
     try:
         db = psycopg2.connect("dbname=catalog")
         cursor = db.cursor()
@@ -12,7 +14,9 @@ def connect():
     except:
         print("Could not connect to database.")
 
+
 def addItem(name, category, description):
+    # adds a new item to the item table
     db, c = connect()
 
     query = "INSERT INTO items(name, category, description) VALUES(%s, %s, %s);"
@@ -23,7 +27,18 @@ def addItem(name, category, description):
     c.close()
     db.close()
 
+def editItem(id, name, category, description):
+    db, c = connect()
+
+    query = "UPDATE items SET name = %s, category = %s, description = %s WHERE id = %s"
+    param = (name, category, description, id)
+    c.execute(query, param)
+    db.commit()
+    c.close()
+    db.close()
+
 def removeItem(id):
+    # remove an item based on its unique id from the item table
     db, c = connect()
 
     query = "DELETE FROM items WHERE id=%s"
@@ -34,7 +49,10 @@ def removeItem(id):
     c.close()
     db.close()
 
+
 def clearDatabase():
+    # delete all items from item table.
+    # for debug purposes only
     db, c = connect()
 
     query = "TRUNCATE items"
@@ -45,19 +63,52 @@ def clearDatabase():
     db.close()
 
 
-
 @app.route('/items')
 def displayItems():
     db, c = connect()
-    
+
+    Item = namedtuple('Item', ['id', 'name', 'category', 'description'])
     query = "SELECT * FROM items"
     c.execute(query)
-    result = c.fetchall()
-    output = ''
-    for i in result:
-        output += str(i)
-        output += '</br>'
-    return output
+    items = c.fetchall()
+    nameditems = [Item(*i) for i in items]
+
+    return render_template('items.html', items=nameditems)
+
+@app.route('/catalog/<category>')
+def categoryItems(category):
+    
+    db, c = connect()
+
+    Item = namedtuple('Item', ['id', 'name', 'category', 'description'])
+    query = "SELECT * FROM items WHERE category=%s"
+    param = (category,)
+    c.execute(query, param)
+    items = c.fetchall()
+    nameditems = [Item(*i) for i in items]
+
+    return render_template('categoryitems.html', items=nameditems, category=category)
+
+@app.route('/catalog/<int:itemid>/<name>')
+def item(itemid, name):
+    # I want to allow for multiple items with the same name
+    db, c = connect()
+
+    Item = namedtuple('Item', ['id', 'name', 'category', 'description'])
+    query = "SELECT * FROM items WHERE id=%s"
+    param = (itemid,)
+    c.execute(query, param)
+    item = Item(*c.fetchone())
+
+    return render_template('item.html', item=item)
+
+@app.route('/catalog/<category>/<int:itemid>/delete', methods=['GET', 'POST'])
+def deleteItem(itemid, category):
+    if(request.method == 'POST'):
+        removeItem(itemid)
+        return redirect(url_for('categoryItems', category=category))
+    else:
+        return render_template('deleteconfirmation.html', itemid=itemid, category=category)
 
 
 @app.route('/')
